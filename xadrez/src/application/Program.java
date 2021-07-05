4f581c229d8954519b61f1c73389fb9c0ed6a699
Fernando <fernando@seo.com.br>
2021-07-05 09:25:30 -0300

    commit terminal

---
+++ xadrez/src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+/**
+ * Created by desenv4 on 05/07/2021.
+ */
+public class Program {
+
+    public static void main(String[] args){
+        System.out.println("Hello Word");
+    }
+
+}
